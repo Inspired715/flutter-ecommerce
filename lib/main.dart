efa4f8804e93ddae2c40f4b9bedb1ef7c26207dc
dev3606 <jd3606192@gmail.com>
2023-09-18 17:13:35 -0400

    Final

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/src/config/route.dart';
+import 'package:flutter_ecommerce_app/src/pages/mainPage.dart';
+import 'package:flutter_ecommerce_app/src/pages/product_detail.dart';
+import 'package:flutter_ecommerce_app/src/widgets/customRoute.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'src/themes/theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce ',
+      theme: AppTheme.lightTheme.copyWith(
+        textTheme: GoogleFonts.mulishTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      routes: Routes.getRoute(),
+      onGenerateRoute: (RouteSettings settings) {
+        if (settings.name.contains('detail')) {
+          return CustomRoute<bool>(
+              builder: (BuildContext context) => ProductDetailPage());
+        } else {
+          return CustomRoute<bool>(
+              builder: (BuildContext context) => MainPage());
+        }
+      },
+      initialRoute: "MainPage",
+    );
+  }
+}
